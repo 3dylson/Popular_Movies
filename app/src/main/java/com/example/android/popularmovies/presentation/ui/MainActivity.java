e323326e5e04b359345c822ea3bd4501916d739b
Edylson Frederico <Edylson_Frederico@outlook.com>
2021-09-28 00:59:48 +0100

    Recycle View Setup

--- app/src/main/java/com/example/android/popularmovies/presentation/ui/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/presentation/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.android.popularmovies;
+package com.example.android.popularmovies.presentation.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.android.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/android/popularmovies/presentation/viewmodels/MoviesViewModel.java
@@ -0,0 +1,4 @@
+package com.example.android.popularmovies.presentation.viewmodels;
+
+public class MoviesViewModel {
+}
